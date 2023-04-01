3bfd876293b24f4679f2ac4a47b92a4974c07950
Jessye Coleman-Shapiro <cjessye@gmail.com>
2023-04-01 15:01:04 -0400

    update background

--- src/Background/index.js
+++ src/Background/index.js
@@ -7,14 +7,14 @@ class Background extends Component {
             <ElementWrap style={{ paddingTop: '20%' }}>
                 <Title>Background</Title>
                 <ContentWrap>
-                    <Text>I am currently entering my fourth year of Computer Engineering at the University of Toronto.
-                    I spent my last 16 months working on the OpenJDK team at
-                    <Link href="https://www.redhat.com/en"> Red Hat</Link> in Toronto.
+                    <Text>
+                        I am currently working as a software developer at <Link href="https://tophat.com/"> Top Hat</Link> in Toronto.
                     </Text>
                     <Text>
-                    Having majored in Visual Arts at the Etobicoke School of the Arts high school,
-                    I carry a unique mix of analytical and design skills. I enjoy tackling challenges of any form
-                    and am always looking for opportunities to gain new experiences.
+                        Having gone to the University of Toronto for Computer Engineering, I have strong
+                        analytical and design skills.  I enjoy tackling challenges of any form
+                        and am always looking for opportunities to gain new experiences.
+                        In my free time I enjoy making pottery, going camping and traveling.
                     </Text>
                 </ContentWrap>
             </ElementWrap>
