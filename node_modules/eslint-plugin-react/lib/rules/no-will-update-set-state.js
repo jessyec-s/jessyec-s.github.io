704fe04f3b1b9d95b1de1a6481c96d0ebab27fee
Jessye Coleman-Shapiro <jessye_coleman_shapiro@Jessyes-MacBook-Pro.local>
2024-11-15 13:42:07 -0500

    update packages

--- node_modules/eslint-plugin-react/lib/rules/no-will-update-set-state.js
+++ node_modules/eslint-plugin-react/lib/rules/no-will-update-set-state.js
@@ -2,8 +2,14 @@
  * @fileoverview Prevent usage of setState in componentWillUpdate
  * @author Yannick Croissant
  */
+
 'use strict';
 
 const makeNoMethodSetStateRule = require('../util/makeNoMethodSetStateRule');
+const testReactVersion = require('../util/version').testReactVersion;
 
-module.exports = makeNoMethodSetStateRule('componentWillUpdate');
+/** @type {import('eslint').Rule.RuleModule} */
+module.exports = makeNoMethodSetStateRule(
+  'componentWillUpdate',
+  (context) => testReactVersion(context, '>= 16.3.0')
+);
